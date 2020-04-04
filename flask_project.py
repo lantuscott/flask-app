8d758775fdeacf83a269fccfbdb37f7198b7e6e4
Marinel Farinha-Guevara <marinelscott@gmail.com>
2020-04-04 18:55:59 -0500

    first commit

---
+++ flask_project.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/hello')
+def hello():
+    return "Hello World!"
+
+@app.route('/jsonInvasion')
+def jsonInvasion():
+    # with open('jason.txt', 'w') as json_file:
+    #     data = json.load(json_file)
+    #     return data
+
+    return render_template("jason.txt")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
